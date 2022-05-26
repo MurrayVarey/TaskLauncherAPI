e17d838a5ae366e2c4fb2ba63c2598cd6308f3a7
MurrayVarey <murray.varey@gmail.com>
2022-05-26 16:55:46 +0100

    Changes Post to Take a List of Files

--- Controller/LaunchController.cs
+++ Controller/LaunchController.cs
@@ -1,4 +1,8 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TaskLauncherAPI.Controllers
@@ -14,10 +18,27 @@ namespace TaskLauncherAPI.Controllers
         }
 
         [HttpPost]
-        public ActionResult<string> PostTaskRequest()
+        public async Task<IActionResult> OnPostUploadAsync(List<IFormFile> files)
         {
+            long size = files.Sum(f => f.Length);
 
-            return "posted";
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.GetTempFileName();
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // Process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size });
         }
     }
 }
