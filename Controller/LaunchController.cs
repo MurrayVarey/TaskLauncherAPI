9668006833905da048e02bef6c5e8a4066afd311
MurrayVarey <murray.varey@gmail.com>
2022-05-26 16:01:42 +0100

    Creates LaunchController.cs
    This controller will be used to launch tasks. It currently just returns a hard-coded result.

---
+++ Controller/LaunchController.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskLauncherAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LaunchController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] {"hardcoded"};
+        }
+    }
+}
